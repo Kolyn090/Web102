559ed9b160a1c311ff878df742b558595bbbe047
Kolyn090 <kolynlin@protonmail.com>
2025-06-19 08:41:55 -0400

    Answer field

--- project3/typeguessor2/src/pages/homepage.jsx
+++ project3/typeguessor2/src/pages/homepage.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import quizdata from "../assets/problemsheet.json"
 import FlashCard from "../components/flashcard"
+import AnswerField from "../components/answerfield"
 import { useState } from "react";
 
 function Homepage(props) {
     const [isFlipped, setIsFlipped] = useState(false);
     const [problemIndex, setProblemIndex] = useState(0);
+    const [feedbackTag, setFeedbackTag] = useState('default');
     const shownProblemIndices = [];
 
     function GetRandomNotShownIndex(list) {
@@ -35,13 +37,19 @@ function Homepage(props) {
         shownProblemIndices.push(randomIndex);
         SetRandomProblem(randomIndex);
         setIsFlipped(false); // Reset flip state on next card
+        setFeedbackTag('default'); // Reset feedback
     };
+
+    const getSolution = () => {
+        return [quizdata[problemIndex]["answer"]];
+    }
 
     return (
         <div>
             <h1>Typescript Type Guessing Game By Kolyn090</h1>
             <h2>How well do you understand types in TypeScript? Take this quiz to find out!</h2>
             <h2>Total number of cards: {quizdata.length}</h2>
+            <p>Current Streak: 0, Longest Streak: 0</p>
             <FlashCard isFlipped={isFlipped}
                         onFlip={handleFlip}
                         ask={quizdata[problemIndex]["ask"]}
@@ -49,6 +57,10 @@ function Homepage(props) {
                         problem={quizdata[problemIndex]["problem"]}
                         answer={quizdata[problemIndex]["answer"]}
             ></FlashCard>
+            <AnswerField feedbackTag={feedbackTag}
+                            setFeedbackTag={setFeedbackTag}
+                            answers={getSolution()}
+            ></AnswerField>
             <button onClick={handleNext} style={{width: '100px'}}>Next</button>
         </div>
     )
